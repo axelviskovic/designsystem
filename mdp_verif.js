0d2b094b7233d443b5e94d32a1ef719ae394b4ab
miroh-toto <andy@ramboloson.mg>
2020-10-24 19:02:58 +0200

    modif javascript pour verif mdp

--- mdp_verif.js
+++ mdp_verif.js
@@ -1,14 +1,32 @@
-function CheckPassword(inputtxt) 
+function CheckPassword() 
 { 
-var passw= "andy";
-if(inputtxt.value.match(passw)) 
-{ 
-alert('Correct, try another...')
-return true;
-}
+    var passw= "andy";
+    var inputPsd = document.getElementById("password");
+      
+    if(inputPsd.value.match(passw)) 
+    { 
+        alert('Mot de passe correct')
+        return true;
+    }
 else
+    { 
+    alert('Mot de passe incorrect')
+    return false;
+    }
+}
+
+/*function CheckEmail() 
 { 
-alert('Wrong...!')
-return false;
-}
-}
+    var email= "andy@gmail.com";
+    var inputEmail = document.getElementById("email");
+    if(inputPsd.value.match(email)) 
+    { 
+        alert('Email correct')
+        return true;
+    }
+else
+    { 
+    alert('Email incorrect')
+    return false;
+    }
+}*/
